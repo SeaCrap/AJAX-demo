85b545bbb379dcd51f34335f3e4902963e191d87
SeaCrap <506076395@qq.com>
2018-10-12 21:21:49 +0800

    JSON.parse(string)：浏览器API 字符串转为对象

--- main.js
+++ main.js
@@ -2,12 +2,29 @@ button.addEventListener('click',(e)=>{
 	let request = new XMLHttpRequest //生成XMLHttpRequest对象
   request.onreadystatechange = ()=>{
     if(request.readyState === 4){
-      console.log('说明请求相应都完毕了')
+     // console.log('说明请求相应都完毕了')
      
       if(request.status >= 200 && request.status < 300){
-        console.log('说明请求成功了')
+       // console.log('说明请求成功了')
         //打印请求内容：request.responseText
+        console.log('1. 服务器返回符合的字符串是')
         console.log(request.responseText)
+        
+        let string = request.responseText
+        //把服务器返回的字符串
+        //转换为符合 JS 对应的值(这里是对象。有可能返回的是数组)
+        let object = window.JSON.parse(string)
+        console.log('2. 打印 object 的类型是') 
+        console.log(typeof object)
+        console.log('3. 打印出 object 是')
+        console.log(object)
+        
+        console.log('4. 打印 onject.note 是')
+        console.log(object.note)
+        
+        console.log('5. 打印 object.note.from 是')
+        console.log(object.note.from)
+     
       }else if(request.status >= 400){
         console.log('说明请求失败了')
       }
