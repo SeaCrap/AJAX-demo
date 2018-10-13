4289fbbaf497e997590e9be057be235c3a69bd98
SeaCrap <506076395@qq.com>
2018-10-14 00:33:36 +0800

    获取响应 header 相关

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ button.addEventListener('click',(e)=>{
  // 请求方式 协议 路径 HOTS   
   request.open('POST','yyy')
  
-  //JS 设置请相应 header(第二部分)  
+  //JS 设置请求 header(第二部分)  
   request.setRequestHeader('mrli','18')
   request.setRequestHeader('Content-Type','x-www-form-urlencocoded')  
   
@@ -14,7 +14,23 @@ button.addEventListener('click',(e)=>{
   
   request.onreadystatechange = ()=>{
     if(request.readyState === 4){
+      
+      //获取响应 状态(status 和 statusText)
+      console.log(request.status)
+      console.log(request.statusText)
+     
       if(request.status >= 200 && request.status < 300){
+        
+        //获取所有响应 header
+        console.log(request.getAllResponseHeaders())
+        // 获取响应 header 中的 content-tepe
+        // 不知道为什么去掉 S 提示错误！
+        console.log(request.getAllResponseHeader('Content-Type'))
+        
+        //获取响应 header 第四部分
+        //删除上边错误才会显示这里
+        console.log(request.responseText)
+        
         let string = request.responseText
         let object = window.JSON.parse(string)
       }else if(request.status >= 400){
